b7a14af1390642754b7f68272480189e2084dc32
philiptaft <ptaft@gia.edu>
2020-04-13 11:11:38 -0700

    updates to inquirer process to confirm final cmd

--- csdt.js
+++ csdt.js
@@ -27,16 +27,13 @@ inquirer
           message: "METHOD:",
           type: "list",
           name: "method",
-          choices: ["Manual Entry", "Process CSV"],
+          choices: ["Follow Prompts", "Read CSV"],
         },
       ])
       .then((selection) => {
         options.method = selection.method;
-        if (selection.method === "Manual Entry") {
-          processManual(options);
-        } else {
-          processCSV(options);
-        }
+        if (selection.method.startsWith("Read")) processCSV(options);
+        else processManual(options);
       });
   })
   .catch((err) => {
@@ -49,7 +46,7 @@ function processManual(options) {
   inquirer
     .prompt([
       {
-        message: "Asset Type:",
+        message: "ASSET TYPE:",
         type: "list",
         name: "assetType",
         choices: [
@@ -63,6 +60,29 @@ function processManual(options) {
           new inquirer.Separator(),
           "GIA_D",
           "GIA_A",
+          new inquirer.Separator(),
+          "GIA_F",
+          "GIA_PD",
+          "GIA_Vocabulary",
+          new inquirer.Separator(),
+          "Dimension",
+          "DimensionSet",
+          new inquirer.Separator(),
+          "GSTAttribute",
+          "GSTDefinition",
+          "GSTProperty",
+          "GSTVirtualWebroot",
+          new inquirer.Separator(),
+          "SiteNavigation",
+          new inquirer.Separator(),
+          "@ALL_NONASSETS",
+          "@ALL_ASSETS",
+          "@SITE",
+          "@ROLE",
+          "@TREETAB",
+          "@STARTMENU",
+          "@ELEMENTCATALOG",
+          "@SITECATALOG",
         ],
       },
     ])
@@ -71,17 +91,33 @@ function processManual(options) {
       inquirer
         .prompt([
           {
-            message: `Enter: <${options.idType}> || '*'`,
-            type: "input",
-            name: "id",
+            message: "CHOOSE:",
+            type: "list",
+            name: "allOrOne",
+            choices: [
+              `a) Execute command '${options.command}' on '${options.assetType}:*'`,
+              `b) Execute command '${options.command}' on '${options.assetType}:<${options.idType}>'`,
+            ],
           },
         ])
         .then((selection) => {
-          options.id = selection.id;
-          /*********************************************/
-          console.log("\nexecuting csdt...\n");
-          runCSDT(options);
-          /*********************************************/
+          if (selection.allOrOne.startsWith("a)")) {
+            options.id = "*";
+            confirmCommand(options);
+          } else {
+            inquirer
+              .prompt([
+                {
+                  message: `INPUT <${options.idType}>:`,
+                  type: "input",
+                  name: "id",
+                },
+              ])
+              .then((selection) => {
+                options.id = selection.id;
+                confirmCommand(options);
+              });
+          }
         });
     });
 }
@@ -98,30 +134,47 @@ function processCSV(options) {
     ])
     .then((selection) => {
       // RegEx to remove/add .csv extension
-      process.env.CSV_FILEPATH = `csv/${selection.filename}.csv`;
+      let filepath = "";
+      if (!selection.filename.startsWith("csv/")) filepath += "csv/";
+      filepath += selection.filename;
+      if (!filepath.endsWith(".csv")) filepath += ".csv";
+      /* *************************************** */
+      process.env.CSV_FILEPATH = filepath;
+      /* *************************************** */
       parser.getResourceStr().then((resString) => {
         options.resources = resString;
-        /*********************************************/
-        console.log("\nexecuting csdt...\n");
-        runCSDT(options);
-        /*********************************************/
+        confirmCommand(options);
       });
     });
 }
 
-function runCSDT(options) {
+function confirmCommand(options) {
   // introduce validation of 'fw-uid' for import and 'cid' for export||listds||listcs
   let cmd;
-  if (options.method === "Manual Entry") {
-    cmd = `${process.env.JAVA_PATH_WINDOWS} -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar developer-tools-command-line-12.2.1.2.0.jar http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.assetType}:${options.id} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
-  } else {
+  if (options.method.startsWith("Read")) {
     cmd = `${process.env.JAVA_PATH_WINDOWS} -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar developer-tools-command-line-12.2.1.2.0.jar http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.resources} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
+  } else {
+    cmd = `${process.env.JAVA_PATH_WINDOWS} -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar developer-tools-command-line-12.2.1.2.0.jar http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.assetType}:${options.id} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
   }
 
-  console.log("************COMMAND:********************");
-  console.log(cmd);
-  console.log("****************************************\n");
+  inquirer
+    .prompt([
+      {
+        message: `RUN CSDT COMMAND: \n"${cmd}"\n`,
+        type: "confirm",
+        name: "exe",
+      },
+    ])
+    .then((selection) => {
+      if (selection.exe) {
+        runCSDT(cmd);
+      } else {
+        console.log("ABORT!");
+      }
+    });
+}
 
+function runCSDT(cmd) {
   console.log("************RESULTS:********************");
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
