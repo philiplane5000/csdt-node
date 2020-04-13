665b50b23b6b21432d4422b04a0d7c8f495630a1
philiptaft <ptaft@gia.edu>
2020-04-13 14:50:15 -0700

    removed 'Page' from options of asset types

--- csdt.js
+++ csdt.js
@@ -51,7 +51,6 @@ function processManual(options) {
         type: "list",
         name: "assetType",
         choices: [
-          "Page",
           "Template",
           "CSElement",
           "SiteEntry",
@@ -154,9 +153,9 @@ function confirmCommand(options) {
   // introduce validation of 'fw-uid' for import and 'cid' for export||listds||listcs
   let cmd;
   if (options.method.startsWith("Read")) {
-    cmd = `${(process.env.JAVA_PATH_WINDOWS) ? process.env.JAVA_PATH_WINDOWS : 'java'}  -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar developer-tools-command-line-12.2.1.2.0.jar http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.resources} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
+    cmd = `${(process.env.JAVA_PATH_WINDOWS) ? process.env.JAVA_PATH_WINDOWS : 'java'}  -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar ${process.env.DEV_TOOLS_COMMAND_LINE_JAR} http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.resources} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
   } else {
-    cmd = `${(process.env.JAVA_PATH_WINDOWS) ? process.env.JAVA_PATH_WINDOWS : 'java'} -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar developer-tools-command-line-12.2.1.2.0.jar http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.assetType}:${options.id} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
+    cmd = `${(process.env.JAVA_PATH_WINDOWS) ? process.env.JAVA_PATH_WINDOWS : 'java'} -Dfile.encoding=UTF-8 -Xbootclasspath/a:lib/servlet-api.jar -jar ${process.env.DEV_TOOLS_COMMAND_LINE_JAR} http://${process.env.WCS_ENV}:80/sites/ContentServer username=${process.env.WCS_USERNAME} password=${process.env.WCS_PASSWORD} resources=${options.assetType}:${options.id} cmd=${options.command} datastore=${process.env.WCS_DATASTORE}`;
   }
 
   inquirer
